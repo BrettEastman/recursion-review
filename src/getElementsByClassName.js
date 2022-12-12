3f3b99e88358e5dff1e02b9c711bddff5bc09d2a
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-12 14:11:25 -0800

    Add first efforts of pair programming recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,33 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// I: class name
+// o: Array list
+// c
+// e
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // declare var result array
+  var resultArr = [];
+
+  // declare inner function
+  var innerFunction = function(element) {
+    // determine base case: does element have classname
+    if (element.classList.contains(className)) {
+      // if so, push into result array
+      resultArr.push(element);
+    }
+    // if element we are currently in has children elements that need to be searched
+    if (element.hasChildNodes()) {
+      element.children.forEach(function(child) {
+        innerFunction(child);
+      });
+    }
+  };
+  // invoke inner function on document.body
+  innerFunction(document.body);
+
+  // return result array
+  return resultArr;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // add undefined and null later
+
+  // if bool
+    // return obj in quotes
+  // if string
+    // return obj in double quotes
+  // if number
+    // return obj in quotes
+  // if array
+    // declare string var
+    // concatenate string with single bracket
+    // forEach to visit each element
+      // add each to the string, then comma
+    // copy slice string 0 to -1
+    // add ending bracket
+    // return string copy
+  // if object
+    // declare string var
+    // opening curly brace
+    // for in loop to visit each
+      // stringifyJSON.key + stringifyJSON.object[key], then comma
+    // copy slice string 0 -1
+    // add ending curly br
+
 };
