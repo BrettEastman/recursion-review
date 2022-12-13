b64fbf78df347d7051704dba6af82bcb392b7672
Brett Austin Eastman <brett.austin.eastman@gmail.com>
2022-12-12 17:16:38 -0800

    Finish assignment for the day

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,40 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  // object handler
+
+  // '{"string":'4',"whatever":[]}
+
+  // array handler
+
+
+  // string handler
+  var stringHandler = function(double) {
+    // '"string"' => 'string'
+    var lastElement = string.length - 1;
+    if (string[0] === '"' && string[lastElement] === '"') {
+      return string.slice(1, -1);
+    }
+  };
+  // '["string","happy"]'
+  // boolean handler
+    // true => true
+  var booleanHandler = function(boolean) {
+    return boolean;
+  }
+
+  // number handler
+  var numberHandler = function (number) {
+    if (Number.isInteger(number) || Number.isFloat(number)) {
+      return number;
+    }
+  };
+
+  // null handler
+  var nullHandler = function() {
+    // 'null' => null
+
+  };
 };
+
